b44bf6c71260d8d007152dbacbaddd24f51378b4
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2020-11-02 14:40:42 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/namesh/flutter_learno/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.namesh.flutter_learno
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_learno/image_picker.dart';
+
+// ignore: non_constant_identifier_names
+bool USE_FIRESTORE_EMULATOR = false;
+
+Future<void> main() async {
+
+  // 🔥 I have to fix the data connection settings. 🔥
+  // ⚠ warning ⚡ high voltage 🚨 police siren
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings = Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  }
+
+  print("The statement 'this machine is connected to the Internet' is: ");
+  print(await DataConnectionChecker().hasConnection);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Upload(),
+    );
+  }
+}
+
